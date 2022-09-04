8a0cb3c6962d2790e37b276a0ba1dfb14a1b10f4
itedya <bdomanski@protonmail.com>
2022-09-05 01:11:10 +0200

    Set up front-end development

---
+++ public/js/app.d.ts
@@ -0,0 +1,3 @@
+export const __esModule: boolean;
+declare function _default(sfc: any, props: any): any;
+export default _default;
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require("laravel-mix");
 
-mix.ts("resources/js/app.ts", "public/js").vue({version: 3})
+mix.js("resources/js/app.js", "public/js").vue({version: 3})
     .css("resources/css/app.css", "public/css");
