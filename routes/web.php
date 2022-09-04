18e974f511d96242feecc94e815104cbbc19d417
itedya <bdomanski@protonmail.com>
2022-09-05 01:00:26 +0200

    Optimized provider, kernel and route files

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix("api")->group(function() {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/auth/revoke', [\App\Http\Controllers\AuthController::class, 'revoke']);
+        Route::get('/auth/user', [\App\Http\Controllers\AuthController::class, 'user']);
+    });
 
+    Route::post('/auth/login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/auth/register', [\App\Http\Controllers\AuthController::class, 'register']);
+});
 Route::view('/{uri}', 'app')->where('uri', '^(?!api).*$');
