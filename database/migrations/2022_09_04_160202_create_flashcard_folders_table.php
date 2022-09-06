65840ed2ec28e5cf0997e06cdf0220e49ff8382f
itedya <bdomanski@protonmail.com>
2022-09-06 21:09:51 +0200

    Added owner_id to flashcard folders migration

--- database/migrations/2022_09_04_160202_create_flashcard_folders_table.php
+++ database/migrations/2022_09_04_160202_create_flashcard_folders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name', 255);
+            $table->foreignId('owner_id')->references('id')->on('users');
 
             $table->timestamps();
         });
