44d0ab53d0afef6e1f6f61e4a836689854c4efa6
itedya <bdomanski@protonmail.com>
2022-09-09 20:07:30 +0200

    Set up CSRF token mechanism

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("api")->group(function() {
+Route::prefix("api")->group(function () {
+    Route::get('/auth/csrf', function () {
+        return csrf_token();
+    });
 
     // auth
     Route::post('/auth/login', [\App\Http\Controllers\AuthController::class, 'login']);
