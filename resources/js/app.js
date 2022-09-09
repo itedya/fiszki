1620b661e76d80ecdbeaa95dd82e8864a0c0f7ec
itedya <bdomanski@protonmail.com>
2022-09-09 20:29:50 +0200

    Added csrf fetch and user fetch on init in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,23 @@
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
+import useAuthStore from "./store/auth.store";
 
-createApp(App)
-    .use(router)
-    .mount("#app");
+const init = async () => {
+    const app = createApp(App)
+        .use(router);
+
+    const authStore = useAuthStore();
+
+    if (!authStore.csrf.value) {
+        await authStore.fetchCSRF();
+    }
+
+    if (authStore.token.value) {
+        await authStore.fetchUser();
+    }
+
+    app.mount("#app");
+}
+
+init();
