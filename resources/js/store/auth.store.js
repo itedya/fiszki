618dbed9280b8b86cceae4cfe714494f5c869ce3
itedya <bdomanski@protonmail.com>
2022-09-10 11:09:32 +0200

    fix: Singleton auth store
    Auth Store was recreating itself at every call, which is not a thing that I wanted. Now it's something like singleton.

--- resources/js/store/auth.store.js
+++ resources/js/store/auth.store.js
@@ -1,41 +1,41 @@
-import api from "./api";
+import api, {setCsrfToApi} from "./api";
 import useLocalstorageDriver from "../composables/localstorage-driver";
 
+const csrf = useLocalstorageDriver("csrf");
+const token = useLocalstorageDriver("token");
+const user = useLocalstorageDriver("user", true);
+
+const fetchCSRF = () => {
+    return api.get("/auth/csrf")
+        .then(res => {
+            csrf.value = res.data;
+            setCsrfToApi(csrf.value);
+            return csrf.value;
+        })
+}
+
+const login = (email, password) => {
+    return api.post("/auth/login", {email, password})
+        .then(res => {
+            const data = res.data;
+            token.value = data.token;
+            return data;
+        });
+}
+
+const register = (email, password) => {
+    return api.post("/auth/register", {email, password});
+}
+
+const fetchUser = () => {
+    return api.get("/auth/user")
+        .then(res => {
+            user.value = res.data;
+            return user.value;
+        });
+}
+
 const useAuthStore = () => {
-    const csrf = useLocalstorageDriver("csrf");
-    const token = useLocalstorageDriver("token");
-    const user = useLocalstorageDriver("user");
-
-    const fetchCSRF = () => {
-        return api.get("/auth/csrf")
-            .then(res => {
-                csrf.value = res.data;
-                api.defaults.headers['X-CSRF-TOKEN'] = csrf.value;
-                return csrf.value;
-            })
-    }
-
-    const login = (email, password) => {
-        return api.post("/auth/login", {email, password})
-            .then(res => {
-                const data = res.data;
-                token.value = data.token;
-                return data;
-            });
-    }
-
-    const register = (email, password) => {
-        return api.post("/auth/register", {email, password});
-    }
-
-    const fetchUser = () => {
-        return api.get("/auth/user")
-            .then(res => {
-                user.value = res.data;
-                return user.value;
-            });
-    }
-
     return {csrf, token, user, fetchUser, register, login, fetchCSRF};
 }
 
