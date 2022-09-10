79bdbcce47020da982b2e3acc17badb69c8e6015
itedya <bdomanski@protonmail.com>
2022-09-10 11:18:24 +0200

    feat: Create route authorization mechanism
    - Created RouteAuth enum
    - Added new enum to routes file

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,13 +1,20 @@
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import RouteAuth from "../enums/route-auth";
 
 export default [
     {
         path: "/",
-        component: Home
+        component: Home,
+        meta: {
+            auth: RouteAuth.MUST_BE_UNAUTHORIZED
+        }
     },
     {
         path: "/auth/login",
-        component: Login
+        component: Login,
+        meta: {
+            auth: RouteAuth.MUST_BE_UNAUTHORIZED
+        }
     }
 ]
