f9d5f3949cbb7791542cfb0db57f9e7bcb0edc97
itedya <bdomanski@protonmail.com>
2022-09-18 13:09:14 +0200

    feat: Added generating default admin user in development environments

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +15,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        if (env("APP_ENV") === "local" || env("APP_ENV") === "testing") {
+            $user = new User();
+            $user->id = 1;
+            $user->email = "localadmin@localhost.local";
+            $user->email_verified_at = now();
+            $user->password = Hash::make("localadmin123");
+            $user->save();
+        }
+
         User::factory(10)->create();
     }
 }
