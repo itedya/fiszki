c33ce0b9cebb4d863c253c6f9358f50cf60a90cc
itedya <bdomanski@protonmail.com>
2022-09-18 17:35:48 +0200

    feat: Added "teach" buttons in Flashcard folder info view

--- resources/js/views/flashcard-folders/Info.vue
+++ resources/js/views/flashcard-folders/Info.vue
@@ -21,12 +21,32 @@ if (id && (flashcardFolder.value === undefined || flashcardFolder.value.flashcar
 const createFlashcard = (data) => {
     flashcardFolder.value.flashcards.push(data);
 }
+
+const teachTraditionally = () => {
+
+}
+
+const teachGradually = () => {
+
+}
 </script>
 
 <template>
-    <div class="container pt-16">
-        <div class="p-5 flex flex-col gap-5" v-if="flashcardFolder">
+    <div class="container pt-16 mx-auto">
+        <div class="p-5 flex flex-col flex-wrap gap-5" v-if="flashcardFolder">
             <h3 class="text-3xl font-extrabold px-3">{{ flashcardFolder.name }}</h3>
+
+            <div class="flex gap-5">
+                <div class="card flex flex-col justify-center items-center w-1/2 square cursor-pointer hover:scale-105 duration-300 hover:shadow" @click="teachGradually">
+                    <p class="font-bold text-2xl">Ucz się</p>
+                    <p class="text-sm">Tryb 5 stopniowy</p>
+                    <p class="text-xs text-yellow-300 text-center">REKOMENDUJEMY</p>
+                </div>
+                <div class="card flex flex-col justify-center items-center w-1/2 square cursor-pointer hover:scale-105 duration-300 hover:shadow" @click="teachTraditionally">
+                    <p class="font-bold text-2xl">Ucz się</p>
+                    <p class="text-sm">Tryb tradycyjny</p>
+                </div>
+            </div>
 
             <div v-for="flashcard of flashcardFolder.flashcards" :key="flashcard.id" class="card">
                 <p>{{ flashcard.front }}</p>
